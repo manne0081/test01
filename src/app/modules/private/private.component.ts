ee0eddb23c7e1364dc159fe01c1e04eb2d1d1b84
manuel-buero <manne0081@gmail.com>
2025-06-06 11:06:51 +0200

    test

--- src/app/modules/private/private.component.ts
+++ src/app/modules/private/private.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 
 import { HeaderComponent } from './header/header.component';
 import { MenuComponent } from './menu/menu.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
     selector: 'app-private',
     standalone: true,
     imports: [
+        RouterModule,
         HeaderComponent,
         MenuComponent,
     ],
