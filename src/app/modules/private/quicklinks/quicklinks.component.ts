b14222eb57015fcb7aa66667c132dde93370b21c
manuel-buero <manne0081@gmail.com>
2025-06-12 10:44:48 +0200

    test

--- src/app/modules/private/quicklinks/quicklinks.component.ts
+++ src/app/modules/private/quicklinks/quicklinks.component.ts
@@ -14,49 +14,6 @@ interface ExampleFlatNode {
     isExpanded?: boolean;
 }
 
-const TREE_DATA: ExampleFlatNode[] = [
-    {
-      name: 'Standard',
-      expandable: true,
-      level: 0,
-    },
-    {
-        name: 'Quicklink 1',
-        expandable: false,
-        level: 1,
-    },
-    {
-        name: 'Quicklink 2',
-        expandable: false,
-        level: 1,
-    },
-    {
-      name: 'Favoriten',
-      expandable: true,
-      level: 0,
-    },
-    {
-      name: 'Quicklink 3',
-      expandable: false,
-      level: 1,
-    },
-    {
-      name: 'Quicklink 4',
-      expandable: false,
-      level: 1,
-    },
-    {
-      name: 'Quicklink 5',
-      expandable: false,
-      level: 1,
-    },
-    {
-      name: 'Quicklink 6',
-      expandable: false,
-      level: 1,
-    },
-];
-
 @Component({
     selector: 'app-quicklinks',
     standalone: true,
@@ -75,38 +32,6 @@ export class QuicklinksComponent {
 
     constructor(
     ) {}
-
-
-
-    treeControl = new FlatTreeControl<ExampleFlatNode>(
-        node => node.level,
-        node => node.expandable,
-    );
-
-    dataSource = new ArrayDataSource(TREE_DATA);
-
-    hasChild = (_: number, node: ExampleFlatNode) => node.expandable;
-
-    getParentNode(node: ExampleFlatNode) {
-        const nodeIndex = TREE_DATA.indexOf(node);
-        for (let i = nodeIndex - 1; i >= 0; i--) {
-            if (TREE_DATA[i].level === node.level - 1) {
-            return TREE_DATA[i];
-            }
-        }
-        return null;
-    }
-
-    shouldRender(node: ExampleFlatNode) {
-        let parent = this.getParentNode(node);
-        while (parent) {
-            if (!parent.isExpanded) {
-            return false;
-            }
-            parent = this.getParentNode(parent);
-        }
-        return true;
-    }
 
     drop(event: CdkDragDrop<string[]>) {
         moveItemInArray(this.quicklinks, event.previousIndex, event.currentIndex);
