4c45b2c65b5e55c50e11be5feeacbfab4535707a
manuel-buero <manne0081@gmail.com>
2025-06-12 11:57:46 +0200

    test

--- src/app/modules/private/quicklinks/quicklinks.component.ts
+++ src/app/modules/private/quicklinks/quicklinks.component.ts
@@ -7,11 +7,10 @@ import {ArrayDataSource} from '@angular/cdk/collections';
 import {FlatTreeControl, CdkTreeModule} from '@angular/cdk/tree';
 
 /** Flat node with expandable and level information */
-interface ExampleFlatNode {
-    expandable: boolean;
+interface TreeNode {
     name: string;
-    level: number;
-    isExpanded?: boolean;
+    children?: TreeNode[];
+    expanded?: boolean;
 }
 
 @Component({
@@ -29,11 +28,47 @@ interface ExampleFlatNode {
 
 export class QuicklinksComponent {
     quicklinks = Array.from({ length: 30 }, (_, i) => `Quicklink ${i + 1}`);
+    tree: TreeNode[] = [
+        {
+          name: 'Standard',
+          expanded: true,
+          children: [
+            { name: 'Leaf 1' },
+            { name: 'Leaf 2' },
+            { name: 'Leaf 3' },
+            { name: 'Leaf 4' },
+          ]
+        },
+        {
+          name: 'Favoriten',
+          expanded: true,
+          children: [
+            { name: 'Leaf 5' },
+            { name: 'Leaf 6' },
+            { name: 'Leaf 7' },
+            { name: 'Leaf 8' },
+            { name: 'Leaf 9' },
+            { name: 'Leaf 10' },
+            { name: 'Leaf 11' },
+            { name: 'Leaf 12' },
+            { name: 'Leaf 13' },
+          ]
+        }
+    ];
 
     constructor(
     ) {}
 
     drop(event: CdkDragDrop<string[]>) {
         moveItemInArray(this.quicklinks, event.previousIndex, event.currentIndex);
+    }
+
+    toggle(node: TreeNode) {
+        node.expanded = !node.expanded;
+    }
+
+    drop2(event: CdkDragDrop<TreeNode[]>, parent: TreeNode) {
+        if (!parent.children) return;
+        moveItemInArray(parent.children, event.previousIndex, event.currentIndex);
     }
 }
