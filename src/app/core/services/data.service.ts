0336d2ae4b8077d161914953a97d0c0f518dc132
manuel-buero <manne0081@gmail.com>
2025-06-23 09:35:08 +0200

    test

--- src/app/core/services/data.service.ts
+++ src/app/core/services/data.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
+
 export class DataService {
 
-  constructor() { }
+    constructor() { }
+
+    truncateText(text: string, maxLength: number): string {
+        if (text.length <= maxLength) {
+            return text;
+        }
+        return text.slice(0, maxLength - 3) + '...';
+    }
 }
--- src/app/modules/private/add-info/add-info.component.ts
+++ src/app/modules/private/add-info/add-info.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PrivateService } from '../private.service';
+import { DataService } from '../../../core/services/data.service';
+import { max } from 'rxjs';
 
 @Component({
     selector: 'app-add-info',
@@ -18,10 +20,15 @@ export class AddInfoComponent {
 
     constructor(
         private privateService: PrivateService,
+        private dataService: DataService,
     ) {
         this.privateService.getSelectedObject().subscribe((data:any) => {
             console.log(data);
             this.addInfoItems = data;
         });
+    }
+
+    truncateText(text: string, maxLength: number): string {
+        return this.dataService.truncateText(text, maxLength);
     }
 }
